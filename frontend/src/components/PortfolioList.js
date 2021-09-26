837d79c7aeec5c8ecda89ef741d092733d8e335b
Min ho Kim <minho42@gmail.com>
2021-09-27 00:20:14 +1000

    show stakeEquityValue in AUD

--- frontend/src/components/PortfolioList.js
+++ frontend/src/components/PortfolioList.js
@@ -8,6 +8,7 @@ const PortfolioList = () => {
   const [stakeEquityValue, setStakeEquityValue] = useState(null);
   const [totalChangeSum, setTotalChangeSum] = useState(0);
   const [totalChangePercentage, setTotalChangePercentage] = useState(0);
+  const [currency, setCurrency] = useState(0);
 
   const isPositive = (str) => {
     return Math.sign(Number.parseFloat(str)) >= 0;
@@ -17,6 +18,17 @@ const PortfolioList = () => {
     return Math.sign(Number.parseFloat(str)) >= 0
       ? `+${Number.parseFloat(str).toLocaleString()}`
       : Number.parseFloat(str).toLocaleString();
+  };
+
+  const fetchCurrencyUsdToAud = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/currency/usdToAud");
+      const { rate } = await res.json();
+      setCurrency(rate);
+    } catch (error) {
+      console.log(error);
+      setCurrency(0);
+    }
   };
 
   const fetchStakeData = async () => {
@@ -53,6 +65,7 @@ const PortfolioList = () => {
 
   useEffect(() => {
     fetchStakeData();
+    fetchCurrencyUsdToAud();
   }, [token]);
 
   return (
@@ -61,11 +74,16 @@ const PortfolioList = () => {
         {stakeEquityValue && (
           <div className="uppercase text-xs tracking-wider">
             Equity value
-            <div className="text-2xl ">
-              ${stakeEquityValue.toLocaleString()}
-              <span className={`ml-2 ${isPositive(totalChangeSum) ? "text-green-600" : "text-red-600"}`}>
-                {showValueWithSign(totalChangeSum)} ({`${showValueWithSign(totalChangePercentage)}%`})
-              </span>
+            <div className="flex text-2xl">
+              <div className="flex">
+                ${stakeEquityValue.toLocaleString()}
+                <div className={`ml-2 ${isPositive(totalChangeSum) ? "text-green-600" : "text-red-600"}`}>
+                  {showValueWithSign(totalChangeSum)} ({`${showValueWithSign(totalChangePercentage)}%`})
+                </div>
+              </div>
+              <div className="ml-3">
+                ≈ A${Number.parseFloat((stakeEquityValue * currency).toFixed(2)).toLocaleString()}
+              </div>
             </div>
           </div>
         )}
