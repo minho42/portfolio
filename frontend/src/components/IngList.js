fb49d4c27e78167becc168bbe75413dd879df30e
Min ho Kim <minho42@gmail.com>
2021-10-07 03:48:08 +1100

    WIP: add ING balance fetching

--- frontend/src/components/IngList.js
+++ frontend/src/components/IngList.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
+import { showValueWithComma } from "../utils";
 
 const IngList = () => {
   const [interestRate, setInterestRate] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [name, setName] = useState(null);
+  const [isBalanceLoading, setIsBalanceLoading] = useState(true);
 
   const fetchInterestRate = async () => {
     try {
@@ -11,16 +15,42 @@ const IngList = () => {
     } catch (error) {}
   };
 
+  const fetchBalance = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/ing/balance");
+      const { data } = await res.json();
+      setBalance(data.balance);
+      setName(data.name);
+      setIsBalanceLoading(false);
+    } catch (error) {
+      setIsBalanceLoading(false);
+    }
+  };
+
   useEffect(() => {
+    fetchBalance();
     fetchInterestRate();
   }, []);
 
   return (
     <div className="flex flex-col px-3 py-3 space-y-3 bg-white rounded-xl">
-      <div className="flex justify-center text-2xl">ING</div>
-      <div className="flex justify-center space-y-2 w-full"></div>
-      <div className="">
-        <div className="text-center">Savings Maximiser: </div>
+      <div className="flex justify-center text-2xl relative">
+        ING
+        <div className="absolute top-0 right-0 text-xs text-gray-500">{name}</div>
+      </div>
+      <div className="flex justify-center space-y-2 w-full">
+        {isBalanceLoading ? (
+          <div className="animate-bounce">Loading...</div>
+        ) : (
+          <div className="uppercase text-xs tracking-wider">
+            Total balance
+            <div className="flex text-2xl">
+              <div className="flex">${showValueWithComma(balance)}</div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
         <div className="text-center text-sm text-gray-500">Interest rate: {interestRate}%</div>
       </div>
     </div>
--- src/routers/ing.js
+++ src/routers/ing.js
@@ -1,8 +1,50 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const puppeteer = require("puppeteer");
+const { login } = require("ing-au-login");
 
-// router.post("/ing/balance", async (req, res) => {});
+const getIngBalance = async () => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      // args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+    });
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on("request", (req) => {
+      if (req.resourceType() == "stylesheet" || req.resourceType() == "font") {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
+    const token = await login(page, process.env.ING_CLIENT_NUMBER, process.env.ING_ACCESS_CODE);
+    await page.waitForSelector(".uia-total-available-balance");
+    const balance = await page.evaluate(() => {
+      return document
+        .querySelector(".uia-total-available-balance > span")
+        .textContent.replace("$", "")
+        .replace(",", "");
+    });
+    const name = await page.evaluate(() => {
+      return document.querySelector("#titleWrapper h1").textContent.replace("Hi ", "").replace(",", "");
+    });
+
+    await browser.close();
+    return { balance, name };
+  } catch (error) {
+    console.log(error);
+    await browser.close();
+    return 0;
+  }
+};
+
+router.get("/ing/balance", async (req, res) => {
+  const data = await getIngBalance();
+  res.send({ data });
+});
 
 router.get("/ing/interestRate", async (req, res) => {
   try {
