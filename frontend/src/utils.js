2c8fed3f51ad101fc0681fc5ecafe5d3ef1ea95d
Min ho Kim <minho42@gmail.com>
2021-10-12 00:08:26 +1100

    add "K"

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -6,6 +6,9 @@ export const showValueWithSign = (str) => {
   return Math.sign(Number.parseFloat(str)) >= 0 ? `+${showValueWithComma(str)}` : showValueWithComma(str);
 };
 
-export const showValueWithComma = (str) => {
+export const showValueWithComma = (str, short = false) => {
+  if (short) {
+    return Number.parseFloat(Number.parseFloat(str / 1000).toFixed(1)).toLocaleString() + "K";
+  }
   return Number.parseFloat(Number.parseFloat(str).toFixed(2)).toLocaleString();
 };
