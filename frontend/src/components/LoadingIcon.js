7536f89261b2e7ccdc7a672a45c818181b25c64c
Min ho Kim <minho42@gmail.com>
2021-10-11 23:59:11 +1100

    change svg to plus-circle

--- frontend/src/components/LoadingIcon.js
+++ frontend/src/components/LoadingIcon.js
@@ -1,6 +1,6 @@
 export const LoadingIcon = () => {
   return (
-    <div className="flex text-gray-400 animate-bounce p-1">
+    <div className="flex text-gray-400 animate-spin p-1">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-6 w-6"
@@ -12,7 +12,7 @@ export const LoadingIcon = () => {
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth={2}
-          d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
     </div>
