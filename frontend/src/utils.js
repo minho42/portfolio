f10e3ce12c9ebd6ef20eb6605de6fa14468365a7
Min ho Kim <minho42@gmail.com>
2021-11-03 01:16:29 +1100

    add dateStrToTimestamp, timestampToDate

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -12,3 +12,13 @@ export const showValueWithComma = (str, short = false) => {
   }
   return Number.parseFloat(Number.parseFloat(str).toFixed(2)).toLocaleString();
 };
+
+export const dateStrToTimestamp = (str) => {
+  // '2021-10-06T13:30:15.312Z' -> 1633527015
+  return Math.round(new Date(str).getTime() / 1000);
+};
+
+export const timestampToDate = (ts) => {
+  // 1633527015 -> '07/10/2021'
+  return new Date(ts * 1000).toLocaleDateString();
+};
